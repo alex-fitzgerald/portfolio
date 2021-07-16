083f285d0fd2dbcaa5e51a403a1f5ceac745aefc
Alex <fitzgerald.s.alexander@gmail.com>
2021-07-16 23:11:39 +1200

    amend package.json

--- server/server.js
+++ server/server.js
@@ -1,7 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const _ = require("lodash")
 const PORT = process.env.port || 3001;
 
 const app = express();
