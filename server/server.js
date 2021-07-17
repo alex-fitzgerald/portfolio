b453e4ed64c2286f061966bd5fcc4758a7ba03ae
Alex <fitzgerald.s.alexander@gmail.com>
2021-07-17 13:01:26 +1200

    amend files to upload to heroku

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
-
 const app = express();
-app.use(express.static('public'));
+const port = process.env.PORT || 3000;
+const path = require('path');
+const publicPath = path.join(__dirname, '..', 'public');
 
-app.get("/api", (req, res) => {
-    res.json({message: "Hello from server!"});
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 });
-
-app.listen(process.env.PORT || 3001, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
