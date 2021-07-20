77373397304671136f3168ffbd2fc0f08b4c7413
Alex <fitzgerald.s.alexander@gmail.com>
2021-07-21 11:40:14 +1200

    amend index to import Poppins

--- src/components/Choice.jsx
+++ src/components/Choice.jsx
@@ -6,8 +6,8 @@ function Choice(props){
     return (
         <div>
             <h1>Here's a snapshot of what I do</h1>
-            <h2>Under Skills you'll find the technologies I use a lot of<br />
-                Under Portfolio you'll a couple of my recent Web and Graphic Design projects</h2>
+            <h3>Under Skills you'll find the technologies I use a lot of</h3>
+            <h3>Under Portfolio you'll a couple of my recent Web and Graphic Design projects</h3>
             <div className="choiceParent">
                 <Button 
                     name="Skills"
