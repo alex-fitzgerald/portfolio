0494ce2520ee869edcac851e2f1e7059f7c349c9
Alex <fitzgerald.s.alexander@gmail.com>
2021-07-21 11:33:48 +1200

    Amend text on profile to make more sense

--- src/components/Choice.jsx
+++ src/components/Choice.jsx
@@ -5,7 +5,9 @@ import Button from './Button.jsx'
 function Choice(props){
     return (
         <div>
-            <h1>Let me show you what I can do</h1>
+            <h1>Here's a snapshot of what I do</h1>
+            <h2>Under Skills you'll find the technologies I use a lot of<br />
+                Under Portfolio you'll a couple of my recent Web and Graphic Design projects</h2>
             <div className="choiceParent">
                 <Button 
                     name="Skills"
