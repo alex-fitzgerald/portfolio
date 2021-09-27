f12b8b6d08d50489ea220858b3bea7aa0d1b7930
Alex <fitzgerald.s.alexander@gmail.com>
2021-09-27 14:16:11 +1300

    Amend styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,16 @@ function Footer(){
     return(
         <div className="footer">
         <div>
-            <img src={profile} alt="Hi!" />
-        </div>
-        <div>
-            <h2>Alex Fitzgerald</h2>
-            <h3>Fullstack Web Developer and Graphic Designer</h3>
-            <a href="mailto:fitzgerald.s.alexander@gmail.com">fitzgerald.s.alexander@gmail.com</a><br />
-            <a rel="noopener noreferrer" href="https://www.linkedin.com/in/alexanderfitzgerald/" target="_blank">Contact me on LinkedIn</a><br />
-            <a href="tel:+6421941738">M: +64 21 941 738</a><br />
+            <div>
+                <img src={profile} alt="Hi!" />
+            </div>
+            <div>
+                <h2>Alex Fitzgerald</h2>
+                <h3>Fullstack Web Developer and Graphic Designer</h3>
+                <a href="mailto:fitzgerald.s.alexander@gmail.com">fitzgerald.s.alexander@gmail.com</a><br />
+                <a rel="noopener noreferrer" href="https://www.linkedin.com/in/alexanderfitzgerald/" target="_blank">Contact me on LinkedIn</a><br />
+                <a href="tel:+6421941738">M: +64 21 941 738</a><br />
+                </div>
             </div>
         </div>
     )
