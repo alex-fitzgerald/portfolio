f6376a07146d8b41068af00902b8cbd134778871
Alex <fitzgerald.s.alexander@gmail.com>
2021-09-30 14:07:51 +1300

    Amend styles and portfolio

--- src/components/Graphic.jsx
+++ src/components/Graphic.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import styles from './Graphic.css'
+
 import fnz1 from '../img/fnz1.jpg'
 import fnz2 from '../img/fnz2.jpg'
 import fnz3 from '../img/fnz3.jpg'
@@ -10,6 +12,12 @@ import rmcv2 from '../img/rmcv2.jpg'
 import rmcv3 from '../img/rmcv3.jpg'
 import datacom1 from '../img/datacom1.jpg'
 import datacom2 from '../img/datacom2.jpg'
+import callumtall from '../img/callum-tall.jpg'
+import webinar from '../img/webinar.jpg'
+import accenture1 from '../img/accenture1.jpg'
+import pulse1 from '../img/coverpage_Page_01.jpg'
+import pulse2 from '../img/coverpage_Page_24.jpg'
+import pulse3 from '../img/coverpage_Page_32.jpg'
 
 function Graphic(){
     return (
@@ -17,6 +25,21 @@ function Graphic(){
                 <h2>Graphic Design</h2>
                 <div className="spread-parent">
                     <div className="spread">
+                        <h3>Report for clients</h3>
+                        <img 
+                            alt="Student Pulse Report, Sample 1" 
+                            src={pulse1}
+                        />
+                        <img 
+                            alt="Student Pulse Report, Sample 2" 
+                            src={pulse2}
+                        />
+                        <img 
+                            alt="Student Pulse Report, Sample 3" 
+                            src={pulse3}
+                        />
+                    </div>
+                    {/* <div className="spread">
                         <h3>FNZ's 2022 Graduate Recruitment Campaign</h3>
                         <img 
                             alt="FNZ Graduate Campaign, Sample 1" 
@@ -26,20 +49,32 @@ function Graphic(){
                             alt="FNZ Graduate Campaign, Sample 2" 
                             src={fnz3}
                         />
+                    </div> */}
+                    <div className="spreadLong">
+                        <h3>More social media assets</h3>
                         <img 
                             alt="FNZ Graduate Campaign, Sample 3" 
-                            src={fnz2}
+                            src={fnz1}
                         />
-                    </div>
-                    <div className="spread">
-                        <h3>Datacom's 2022 Graduate Recruitment Campaign support</h3>
+                        <img 
+                            alt="Datacom Graduate Campaign, Sample 1" 
+                            src={callumtall}
+                        />
                         <img 
                             alt="Datacom Graduate Campaign, Sample 1" 
                             src={datacom1}
                         />
                         <img 
+                            alt="RMCV Graduate Campaign, Sample 1" 
+                            src={rmcv1}
+                        />
+                        <img 
                             alt="Datacom Graduate Campaign, Sample 2" 
-                            src={datacom2}
+                            src={accenture1}
+                        />
+                        <img 
+                            alt="Datacom Graduate Campaign, Sample 2" 
+                            src={webinar}
                         />
                     </div>
                     <div className="spread">
@@ -58,12 +93,8 @@ function Graphic(){
                             src={edith1}
                         />
                     </div>
-                    <div className="spread">
+                    {/* <div className="spread">
                         <h3>Russell McVeagh's 2021 Graduate Recruitment Campaign</h3>
-                        <img 
-                            alt="RMCV Graduate Campaign, Sample 1" 
-                            src={rmcv1}
-                        />
                         <img 
                             alt="RMCV Graduate Campaign, Sample 2" 
                             src={rmcv3}
@@ -72,7 +103,7 @@ function Graphic(){
                             alt="RMCV Graduate Campaign, Sample 3" 
                             src={rmcv2}
                         />
-                    </div>
+                    </div> */}
                 </div>
             </div>
     )
